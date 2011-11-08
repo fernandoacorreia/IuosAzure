eba4902914351dcdfafa99f0e1341a5d7823e655
Fernando Correia <fernandoacorreia@gmail.com>
2011-11-08 10:01:30 -0200

    esqueleto de Data Service

--- src/IousAzure.Entidades/ContextoBd.cs
+++ src/IousAzure.Entidades/ContextoBd.cs
@@ -6,6 +6,12 @@ namespace IousAzure.Entidades
 {
     public class ContextoBd : DbContext
     {
+        public ContextoBd()
+        {
+            // Disable proxy creation as this messes up the data service.
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
         public DbSet<Inquilino> Inquilinos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Solicitacao> Solicitacoes { get; set; }
---
+++ src/IousAzure.Servico/WcfDataService1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using System.Data.Objects;
+using IousAzure.Entidades;
+using System.Data.Entity.Infrastructure;
+
+namespace IousAzure.Servico
+{
+    // http://code.msdn.microsoft.com/Code-First-Northwind-Data-874c947b
+    // http://social.technet.microsoft.com/wiki/contents/articles/5234.aspx
+    public class WcfDataService1 : DataService<ObjectContext>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // We need to explicitly set the less restrictive rights.
+            config.SetEntitySetAccessRule("Inquilinos", EntitySetRights.All);
+
+            // Set the remaining entity sets to read all.
+            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+
+        // We must override CreateDataSource to manually return an ObjectContext,
+        // otherwise the runtime tries to use the built-in reflection provider instead of 
+        // the Entity Framework provider.
+        protected override ObjectContext CreateDataSource()
+        {
+            ContextoBd contextoBd = new ContextoBd();
+
+            // Configure DbContext before we provide it to the 
+            // data services runtime.
+            contextoBd.Configuration.ValidateOnSaveEnabled = false;
+
+            // Get the underlying ObjectContext for the DbContext.
+            var objectContext = ((IObjectContextAdapter)contextoBd).ObjectContext;
+
+            // Return the underlying context.
+            return objectContext;
+        }
+    }
+}
